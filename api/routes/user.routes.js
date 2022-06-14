2c1d55e1acf23a0ebdeac7e462114f57d69c469c
leandro <martinezleandro2004@gmail.com>
2022-06-14 11:55:36 -0300

    added middleware

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -6,10 +6,15 @@ const {
   updateUser,
   deleteUser,
 } = require("../controllers/user.controller");
+const { verifySignup } = require("../middlewares/index");
 
 router.get("/", getUsers);
 router.get("/:userId", getOneUser);
-router.post("/", createUser);
+router.post(
+  "/",
+  [verifySignup.checkDuplicateEmail, verifySignup.validatePassword],
+  createUser
+);
 router.put("/:userId", updateUser);
 router.delete("/:userId", deleteUser);
 
