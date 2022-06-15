23b4b88c21407d91c960153ff60448693998372f
leandro <martinezleandro2004@gmail.com>
2022-06-14 21:05:35 -0300

    added vercel.json file

--- api/database/db.js
+++ api/database/db.js
@@ -1,8 +1,10 @@
 const Sequelize = require("sequelize");
-
+const pg = require("pg");
+require("dotenv");
 const sequelize = new Sequelize("challengealkemy", "postgres", "root", {
   host: "localhost",
   dialect: "postgres",
+  dialectModule: pg,
 });
 
 module.exports = {
