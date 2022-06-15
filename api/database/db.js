b4ca476e37e1753974b85ab02cd0fe60181070db
leandro <martinezleandro2004@gmail.com>
2022-06-14 21:27:02 -0300

    .env

--- api/database/db.js
+++ api/database/db.js
@@ -1,11 +1,16 @@
 const Sequelize = require("sequelize");
 const pg = require("pg");
-require("dotenv");
-const sequelize = new Sequelize("challengealkemy", "postgres", "root", {
-  host: "localhost",
-  dialect: "postgres",
-  dialectModule: pg,
-});
+require("dotenv").config();
+const sequelize = new Sequelize(
+  process.env.DB,
+  "postgres",
+  process.env.password,
+  {
+    host: "localhost",
+    dialect: "postgres",
+    dialectModule: pg,
+  }
+);
 
 module.exports = {
   sequelize,
